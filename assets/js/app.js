3e4f2de98cdfbee2b88c56061f8eec0b19126492
Benjamin Thor <benjamin@dnx-team.com>
2016-10-20 02:47:33 +0200

    Adds audio control for iOS users

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,24 @@
-// Nothing 
+var $ = require('jquery');
+
+$(document).ready(function(){
+    var audio     = document.getElementById('audio');
+    var userAgent = window.navigator.userAgent;
+    var isIOS     = userAgent.match(/iPad/i) || userAgent.match(/iPhone/i);
+
+    if (!isIOS) {
+        $('.audio-control').hide();
+    }
+
+    $('.audio-control').on('click', function(e){
+        e.preventDefault();
+        var $this = $(this);
+
+        $this.toggleClass('is-playing');
+
+        if ($this.hasClass('is-playing')) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
